3a52251e0a6d45659ec16c90c5ebc5320f50b33b
MohammadMahdi Arbabpouri <sardarcyberym@gmail.com>
2023-04-13 21:28:13 +0330

    update

--- modules/functions/payment_link.py
+++ modules/functions/payment_link.py
@@ -1,4 +1,6 @@
 from config import Config
 
 def create_payment_link(UserId: int, Price: int) -> str:
+    # salam
+    # allaho akbar
     return "{}/?userId={}&Price={}".format(Config.PAYMENT_DOMAIN, UserId, Price)
