5989ff28b8b9a093d737d17db1fd3b4193692674
MohammadMahdi Arbabpouri <sardarcyberym@gmail.com>
2023-05-18 14:43:25 +0330

    create models

--- modules/models/models.py
+++ modules/models/models.py
@@ -46,15 +46,7 @@ class GetAllConfigTypesList(BaseModel):
 class GetAllConfigTypes(BaseModel):
     status: int
     message: str
-    result: GetAllConfigTypesList = None
-
-
-#
-class GetUserConfigTypes(BaseModel):
-    pass
-
-
-
+    result: Optional[GetAllConfigTypesList] = None
 
 
 #
@@ -67,4 +59,182 @@ class OfflineCharge(BaseModel):
 
 
 
+# 
+class GetConfigResult(BaseModel):
+    id: int
+    name: str
+    protocol: str
+    serverName: str
+    configTypeTitle: str
+    numberOfUsers: int
+    activeDays: int
+    maxTraffic: int
+    v2RayLink: str
+    creationDate: str # TODO
+    expiresDate: str # TODO
+    clientUserId: int
+    up: int
+    down: int
+    isEnable: bool
+
+class GetConfig(BaseModel):
+    status: int
+    message: str
+    result: Optional[GetConfigResult] = None
+
+
+#
+class GetUserConfigsResult(BaseModel):
+    id: int
+    name: str
+    protocol : str
+    serverName: str
+    configTypeTitle: str
+    numberOfUsers: int
+    activeDays: int
+    maxTraffic: int
+    v2RayLink: str
+    creationDate: str # TODO
+    expiresDate: str # TODO
+    clientUserId: int
+    up: int
+    down: int
+    isEnable: bool
+
+
+class GetUserConfigs(BaseModel):
+    status: int
+    message: str
+    result: Optional[List[GetUserConfigsResult]] = None
+
+
+#
+class AddNewConfigResult(BaseModel):
+    name: str
+    protocol: str
+    serverName: str
+    configTypeTitle: str
+    numberOfUsers: int
+    activeDays: int
+    maxTraffic: int
+    v2RayLink: str
+    creationDate: str # TODO
+    expiresDate: str # TODO
+
+
+class AddNewConfig(BaseModel):
+    status: int
+    message: str
+    result: Optional[AddNewConfigResult] = None
+
+
+#
+class ChangeProtocolResult(BaseModel):
+    name: str
+    protocol: str
+    serverName: str
+    configTypeTitle: str
+    numberOfUsers: int
+    activeDays: int
+    maxTraffic: int
+    v2RayLink: str
+    up: int
+    down: int
+    creationDate: str # TODO
+    expiresDate: str # TODO
+
+
+class ChangeProtocol(BaseModel):
+    status: int
+    message: str
+    result: Optional[ChangeProtocolResult] = None
+
+
+#
+class ChangeServerResult(BaseModel):
+    name: str
+    protocol: str
+    serverName: str
+    configTypeTitle: str
+    numberOfUsers: int
+    activeDays: int
+    maxTraffic: int
+    v2RayLink: str
+    up: int
+    down: int
+    creationDate: str # TODO
+    expiresDate: str # TODO
+
+class ChangeServer(BaseModel):
+    status: int
+    message: str
+    result: Optional[ChangeServerResult] = None
+
+
+
+#
+class RenewakConfigResult(BaseModel):
+    name: str
+    protocol: str
+    serverName: str
+    configTypeTitle: str
+    numberOfUsers: int
+    activeDays: int
+    maxTraffic: int
+    v2RayLink: str
+    up: int
+    down: int
+    creationDate: str # TODO
+    expiresDate: str # TODO
+
+class RenewakConfig(BaseModel):
+    status: int
+    message: str
+    result: Optional[RenewakConfigResult] = None
+
+
+#
+class DeleteConfig(BaseModel):
+    status: int
+    message: str
+    result: Optional[str] = None
+
+
+#
+class GetAllServerResult(BaseModel):
+    id: int
+    name: str
+    limit: int
+    activeConfigs: int
+
+class GetAllServerList(BaseModel):
+    servers: Optional[List[GetAllServerResult]] = None
+
+class GetAllServer(BaseModel):
+    statis: int
+    message: str
+    result: Optional[GetAllServerList] = None
+
+
+#
+class GetSettingsResult(BaseModel):
+    minSellerAmount: int
+
+class GetSettings(BaseModel):
+    status: int
+    message: str
+    result: Optional[GetSettingsResult]
+
+
+#
+class GetUserInfoResult(BaseModel):
+    balance: int
+    referralerUserId: Optional[int] = None
+    referrals: Optional[List[int]] = None
+    precentage: int
+
+class GetUserInfo(BaseModel):
+    status: int
+    message: str
+    result: Optional[GetUserInfoResult] = None
 
